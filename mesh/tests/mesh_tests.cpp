110439f7390f49e3b9f6b31cc0385e946e2991e0
Bob Robey <brobey@lanl.gov>
2014-09-29 09:32:57 -0600

    Syncing up mesh tests

--- mesh/tests/mesh_tests.cpp
+++ mesh/tests/mesh_tests.cpp
@@ -2,10 +2,21 @@
 #include "l7.h"
 #include "mpi.h"
 
-#ifdef HAVE_CL_DOUBLE
-typedef double real_t;
-#else
-typedef float real_t;
+#if !defined(FULL_PRECISION) && !defined(MIXED_PRECISION) && !defined(MINIMUM_PRECISION)
+#define FULL_PRECISION
+#endif
+#ifdef NO_CL_DOUBLE
+#undef  FULL_PRECISION
+#undef  MIXED_PRECISION
+#define MINIMUM_PRECISION
+#endif
+
+#if defined(MINIMUM_PRECISION)
+   typedef float real_t; // this is used for intermediate calculations
+#elif defined(MIXED_PRECISION) // intermediate values calculated high precision and stored as floats
+   typedef double real_t;
+#elif defined(FULL_PRECISION)
+   typedef double real_t;
 #endif
 
 enum partition_method initial_order = HILBERT_SORT;
@@ -19,8 +30,8 @@ int main (int argc, char **argv)
    //  Process command-line arguments, if any.
    int mype=0;
    int numpe=0;
-   int do_quo_setup=0;
-   int lttrace_on=0;
+   int do_quo_setup = 0;
+   int lttrace_on = 0;
    L7_Init(&mype, &numpe, &argc, argv, do_quo_setup, lttrace_on);
 
    int nx = 4;
@@ -33,24 +44,26 @@ int main (int argc, char **argv)
    double circ_radius = 6.0;
    circ_radius *= (double)nx / 128.0; // scaling radius for problem size
 
-   mesh = new Mesh(nx, ny, levmx, ndim, boundary, parallel_in, do_gpu_calc);
+   double deltax_in = 1.0;
+   double deltay_in = 1.0;
+   mesh = new Mesh(nx, ny, levmx, ndim, deltax_in, deltay_in, boundary, parallel_in, do_gpu_calc);
 
    mesh->init(nx, ny, circ_radius, initial_order, do_gpu_calc);
 
    MallocPlus state_memory;
 
    real_t *density = (real_t *)state_memory.memory_malloc(mesh->ncells, sizeof(real_t), "density");
-   for (uint ic=0; ic<mesh->ncells; ic++){
+   for (int ic=0; ic<mesh->ncells; ic++){
       density[ic]=1.0;
    }
 
    mesh->do_load_balance_local(mesh->ncells, NULL, state_memory);
 
    int ncells_test = mesh->ncells_global/mesh->numpe;
-   if ((int)mesh->ncells_global%mesh->numpe > mype) ncells_test++;
+   if (mesh->ncells_global%mesh->numpe > mype) ncells_test++;
 
    int ierr = 0;
-   if (ncells_test != (int)mesh->ncells) ierr = 1;
+   if (ncells_test != mesh->ncells) ierr = 1;
 
    int ierr_global = 0;
    MPI_Allreduce(&ierr, &ierr_global, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
